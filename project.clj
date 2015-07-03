893d69c3d4854e6e346237b6cd0bd2541d282302
Daniel Compton <desk@danielcompton.net>
2015-07-04 11:15:29 +1200

    Upgrade to 1.7 and rename files to cljc

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
           :src-dir-uri "https://github.com/apa512/clj-rethinkdb/blob/master/"
           :src-linenum-anchor-prefix "L"}
   :global-vars {*warn-on-reflection* true}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/data.json "0.2.6"]
                  [org.flatland/protobuf "0.8.1"]
